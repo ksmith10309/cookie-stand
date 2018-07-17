398f8e2e2bf700ccb20bb1e18ccd1112aef20647
Katherine Smith <katherine.lo.smith@gmail.com>
2018-07-17 15:18:06 -0700

    Change format for each hour

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
 var allStores = [];
 
@@ -59,6 +59,7 @@ var tableEl = document.createElement('table');
 
 //create heading row
 var trElHeading = document.createElement('tr');
+trElHeading.setAttribute('id', 'tableHeading');
 var thElEmpty = document.createElement('th');
 trElHeading.appendChild(thElEmpty);
 
